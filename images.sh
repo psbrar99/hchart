827c8d1608c6d9a56369c50dd3a8515bb6524e98
Prabhjot Singh <86856448+psbrar99@users.noreply.github.com>
2023-08-25 16:31:46 -0600

    Update images.sh

--- images.sh
+++ images.sh
@@ -18,6 +18,6 @@ IMAGE_SUFFIXES=("" "-debug" "-distroless")
 for image in "${IMAGES[@]}"; do
   for suffix in "${IMAGE_SUFFIXES[@]}"; do
     DIGEST=$(crane digest $HUB/${image}:${TAG}${suffix})
-    cosign sign --identity-token=$(${ACCESS_TOKEN} --audiences=sigstore --include-email --impersonate-service-account image-signing-keyless-sa@tid-testing.iam.gserviceaccount.com)
+    cosign sign -y --identity-token=$(${ACCESS_TOKEN} --audiences=sigstore --include-email --impersonate-service-account image-signing-keyless-sa@tid-testing.iam.gserviceaccount.com) $HUB/${image}@$digest
   done
 done
